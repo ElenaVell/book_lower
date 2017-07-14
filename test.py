2dd0f5fb850a424e454ca8c7543a17a0f9bc0ead
Sergey Vasilenko <svasilenko@mirantis.com>
2017-07-14 14:19:22 +0300

    stylistic fixes from /sv

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 from db import User
 
 def users_url():
-	
-	u=User
-	b=[]
-	for i in range(99):
-		a=u.query.get(i+1)
-		b.append(a.profile_url)
-	return b
-	
-links = users_url()	
-print(links)
+    u = User
+    b = []
+    for i in range(99):
+        a = u.query.get(i+1)
+        b.append(a.profile_url)
+    return b
+
+if __name__ == "__main__":
+    links = users_url()
+    print(links)
